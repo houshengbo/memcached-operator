6c00268e86735b99fdde76b72e6a3b803aab5de0
Vincent Hou <shou@us.ibm.com>
2021-03-10 14:30:58 -0500

    Create v1beta1 CRD

--- README.md
+++ README.md
@@ -31,3 +31,16 @@ Create the CR:
 ```
 kubectl apply -f config/samples/cache_v1alpha1_memcached.yaml
 ```
+
+Create the operator with v1beta1 CRD
+The v1beta1 CRD has the same content as v1alpha1 CRD.
+
+```
+operator-sdk create api --group cache --version v1beta1 --kind Memcached
+```
+
+Only create the resource, not the controller, because we will change the existing one.
+The principle is that each controller will only reconcile on one version of the CR.
+
+
+
--- main.go
+++ main.go
@@ -32,6 +32,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	cachev1alpha1 "github.com/example/memcached-operator/api/v1alpha1"
+	cachev1beta1 "github.com/example/memcached-operator/api/v1beta1"
 	"github.com/example/memcached-operator/controllers"
 	// +kubebuilder:scaffold:imports
 )
@@ -45,6 +46,7 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(cachev1alpha1.AddToScheme(scheme))
+	utilruntime.Must(cachev1beta1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
